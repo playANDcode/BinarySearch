2f93b3cf120e7bf6447c735b1f39b84a3ce692b1
playANDcode <85435538+playANDcode@users.noreply.github.com>
2021-09-09 14:34:51 +0800

    Fixed Bug #1

--- First_Attempt.c
+++ First_Attempt.c
@@ -3,9 +3,8 @@
 /*
 We are not removing values in array that are to be disregarded
 Instead, we assume it by making the program think 
-    -  we are reducing the size of the array
-    -  making index 5 or index 3, any index number as index 0 
-I am using the word "ASSUME" because technically, we aren't removing any values at all. 
+    -  we are reducing the array
+    -  making index 5 or index 3, any number as index 0 for example
 */
 
 int main(void)
@@ -13,7 +12,7 @@ int main(void)
     // Values
     int numbers[9] = {1, 3, 4, 6, 7, 8, 10, 13, 14};
     int arr_length = sizeof(numbers) / sizeof(int);
-    int index_resetter = 0;  // Value that assumes index 5,3 any index number to be index 0
+    int index_resetter = 0;  // Value that assumes index 5 to be index 0
     int half = 0;           // The location of middle index in array
     int middle_value;       // The value of the middle index
 
@@ -26,22 +25,22 @@ int main(void)
     while (1)
     {
         arr_length -= half;     // Assumes that len of array is being reduced
-        half = arr_length / 2;  
+        half = arr_length / 2;
         middle_value = numbers[half + index_resetter]; 
         if (middle_value == given_number)
         {
             printf("Found!!");
             return 0;
         }
-        // middle_value + 1 means right side of the selected array
-        else if (middle_value + 1 <= given_number)
-        {
-            index_resetter += half;
-        }
-        else
+        // Half == 0 means the array has been fully searched
+        else if (half == 0)
         {
             printf("Digit %i doesn't exist in the array", given_number);
             return 1;
+        }
+        else if (middle_value <= given_number)
+        {
+            index_resetter += half;
         }
     }
 }
